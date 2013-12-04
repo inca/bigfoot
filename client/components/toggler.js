71e6513f41e14aadfbea9bbec1b842e65bb98d96
Boris Okunskiy <i@savant.pro>
2013-12-04 22:11:22 +0400

    Implemented another strategy for togglers initializing. Really cool.

--- client/components/toggler.js
+++ client/components/toggler.js
@@ -1,10 +1,10 @@
 $.bigfoot.install('.toggler', function() {
 
-  var e = $(this);
+  var toggler = $(this);
 
-  var buttons = $("*[data-for]", e);
+  var buttons = $("*[data-for]", toggler);
 
-  var cssClass = e.attr("data-toggler-class");
+  var cssClass = toggler.attr("data-toggler-class");
   if (!cssClass)
     cssClass = "active";
 
@@ -18,14 +18,18 @@ $.bigfoot.install('.toggler', function() {
     });
   }
 
+  function hideAll() {
+    buttons.each(function() {
+      $($(this).attr("data-for")).hide();
+    });
+  }
+
   buttons.unbind(".bigfoot.toggler")
     .bind("click.bigfoot.toggler", function(ev) {
       ev.preventDefault();
       var b = $(this);
       // hide all targets
-      buttons.each(function() {
-        $($(this).attr("data-for")).hide();
-      });
+      hideAll();
       // show this one
       $(b.attr("data-for")).show();
       // update buttons state
@@ -33,5 +37,12 @@ $.bigfoot.install('.toggler', function() {
       return false;
     });
 
-  updateButtons();
+  // Init from markup -- or by reading button classes
+  var active = buttons.filter('.' + cssClass);
+  if (active.size() == 0) // Init toggler by reading related markup
+    updateButtons();
+  else { // Hide all targets except initial
+    hideAll();
+    $(active.attr('data-for')).show();
+  }
 });
