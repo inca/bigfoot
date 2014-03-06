9e40203f5760ae87269f936ee40af7d91fc4e5c9
Boris Okunskiy <i@savant.pro>
2014-03-06 23:42:00 +0400

    Removed grunt-jade

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,32 +32,13 @@ module.exports = function(grunt) {
         src: 'build/bigfoot-noinit-<%=pkg.version%>.js',
         dest: 'build/bigfoot-noinit-<%=pkg.version%>.min.js'
       }
-    },
-
-    jade: {
-      compile: {
-        options: {
-          pretty: true,
-          data: {
-            pkg: pkg,
-            rho: function(file) {
-              var text = fs.readFileSync(file, { encoding: 'utf-8' });
-              return rho.toHtml(text);
-            }
-          }
-        },
-        files: {
-          "index.html": "pages/index.jade"
-        }
-      }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jade');
 
-  grunt.registerTask('default', ['browserify', 'uglify', 'jade']);
+  grunt.registerTask('default', ['browserify', 'uglify']);
 
 };
