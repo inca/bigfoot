7da9c416ee0cd1d2bb65af043bdd287c6ebcc756
Boris Okunskiy <i@savant.pro>
2014-07-05 11:04:45 +0400

    Pimped sticky with RAF.

--- build/bigfoot-noinit.js
+++ build/bigfoot-noinit.js
@@ -591,25 +591,53 @@ $.bigfoot.install('[data-load]', function() {
 
 },{}],12:[function(require,module,exports){
 $.bigfoot.install('[data-sticky]', function() {
-  var sticky = $(this)
-    , parent = $(this.parentNode)
-    , wnd = $(window)
-    , oldY = 0
+  var sticky = this
+    , $sticky = $(this)
+    , $parent = $(this.parentNode)
+    , $wnd = $(window)
     , enabled = true
-    , continuous = $(this).attr('data-sticky') == 'continuous';
-  function update() {
+    , updating = false
+    , raf = window.requestAnimationFrame;
+
+  var oldY, viewTop, windowHeight, stickyHeight, parentTop, parentBottom;
+
+  function reinit() {
+    var _float = $sticky.css('float');
+    var _enabled = _float == 'left' || _float == 'right';
+    // Handle special case where "switching" occurs
+    if (enabled != _enabled) {
+      $sticky.removeAttr('style');
+      oldY = 0; // TODO remove this?
+    }
+    enabled = _enabled;
+    oldY = 0;  // TODO Or this?
+    viewTop = $wnd.scrollTop();
+    windowHeight = $wnd.height();
+    stickyHeight = $sticky.height();
+    parentTop = $parent.offset().top;
+    parentBottom = parentTop + $parent.height();
+    update();
+  }
+
+  function onScroll() {
     if (!enabled)
       return;
+    viewTop = $wnd.scrollTop();
+    requestUpdate();
+  }
+
+  function requestUpdate() {
+    if (!updating) raf(update);
+    updating = true;
+  }
+
+  function update() {
     var newY = oldY
-      , windowHeight = wnd.height()
-      , stickyHeight = sticky.height()
-      , viewTop = wnd.scrollTop()
       , viewBottom = viewTop + windowHeight
-      , parentTop = parent.offset().top
-      , parentBottom = parentTop + parent.height()
       , boundTop = Math.max(viewTop + 16, parentTop) - parentTop
       , boundBottom = Math.min(viewBottom - 16, parentBottom) - parentTop
       , alwaysAtTop = stickyHeight < windowHeight && boundTop + stickyHeight < boundBottom;
+    updating = false;
     if (boundTop > oldY && boundBottom < oldY + stickyHeight)
       return;
     if (boundTop < oldY || alwaysAtTop) {
@@ -619,29 +647,20 @@ $.bigfoot.install('[data-sticky]', function() {
     }
     if (newY != oldY) {
       oldY = newY;
-      sticky.css('transform', 'translateY(' + oldY + 'px)')
-        .css('-moz-transform', 'translateY(' + oldY + 'px)')
-        .css('-webkit-transform', 'translateY(' + oldY + 'px)');
+      sticky.style.webkitTransform =
+        sticky.style.MozTransform =
+          sticky.style.msTransform =
+            sticky.style.OTransform =
+              sticky.style.transform =
+                'translateY(' + oldY + 'px)';
     }
   }
-  function reinit() {
-    var _float = sticky.css('float');
-    var _enabled = _float == 'left' || _float == 'right';
-    // Handle special case where "switching" occurs
-    if (enabled != _enabled) {
-      sticky.removeAttr('style');
-      oldY = 0;
-    }
-    enabled = _enabled;
-    update();
-  }
+
   // Bind to events
   reinit();
-  if (continuous)
-    wnd.scroll(update);
-  else
-    wnd.scrollStop(update);
-  wnd.resizeStop(reinit);
+  $wnd.resizeStop(reinit);
+  $wnd.scroll(onScroll);
+
 });
 },{}],13:[function(require,module,exports){
 $.bigfoot.switcher = {
